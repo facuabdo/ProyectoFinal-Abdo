e0f3ab0f3cc01209787a60714e151613e28d6568
Facundo Abdo <facundo.abdo@globant.com>
2024-01-15 09:33:17 -0300

    firestore init

--- src/config/firebase.jsx
+++ src/config/firebase.jsx
@@ -1,6 +1,8 @@
-import { getFirestore } from "firebase/firestore";
+import { addDoc, collection, getDocs, getFirestore } from "firebase/firestore";
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { productos } from "../data/dataMock";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -16,3 +18,22 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
+
+export const initializeEcommFirestore = () => {
+  // Consulta la colección
+  const collectionRef = collection(db, "productos");
+  getDocs(collectionRef)
+    .then((querySnapshot) => {
+      if (querySnapshot.empty) {
+        console.log("La colección no existe o está vacía");
+        productos.forEach((prod) => {
+          addDoc(collectionRef, prod).then((docref) => {
+            console.log(`se agergo el documento con id: ${docref.id}`);
+          });
+        });
+      }
+    })
+    .catch((error) => {
+      console.log("Error al obtener la colección:", error);
+    });
+};
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import App from "./App.jsx";
 import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { initializeEcommFirestore } from "./config/firebase.jsx";
+
+initializeEcommFirestore();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
